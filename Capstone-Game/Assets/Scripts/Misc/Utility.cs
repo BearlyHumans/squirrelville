f27e1e5dabb96a8bb21ce5c90d8064d35bb0104c
Jordan Epstein <102747419@student.swin.edu.au>
2021-04-14 15:19:04 +1000

    Revert "Simplified code and removed redundant methods"
    This reverts commit 0c7620ec41bd92ed4e074c47e31f0780dee64b29.

--- Capstone-Game/Assets/Scripts/Misc/Utility.cs
+++ Capstone-Game/Assets/Scripts/Misc/Utility.cs
@@ -7,68 +7,132 @@ public static class Utility
     /// <summary> Return true if this float has a greater unsigned value than the comparison float. </summary>
     public static bool FurtherFromZero(this float f, float comparison)
     {
-        return (comparison > 0 && f > comparison) || (comparison < 0 && f < comparison);
+        if ((comparison > 0 && f > comparison) || (comparison < 0 && f < comparison))
+            return true;
+        return false;
     }
 
     /// <summary> Return true if this int has a greater unsigned value than the comparison int. </summary>
     public static bool FurtherFromZero(this int f, int comparison)
     {
-        return (comparison > 0 && f > comparison) || (comparison < 0 && f < comparison);
+        if ((comparison > 0 && f > comparison) || (comparison < 0 && f < comparison))
+            return true;
+        return false;
     }
 
     /// <summary> Return true if this float has a smaller unsigned value than the comparison float. </summary>
     public static bool CloserToZero(this float f, float comparison)
     {
-        return (comparison > 0 && f <= comparison) || (comparison < 0 && f >= comparison);
+        if ((comparison > 0 && f <= comparison) || (comparison < 0 && f >= comparison))
+            return true;
+        return false;
     }
 
     /// <summary> Return true if this int has a smaller unsigned value than the comparison int. </summary>
     public static bool CloserToZero(this int f, int comparison)
     {
-        return (comparison > 0 && f <= comparison) || (comparison < 0 && f >= comparison);
+        if ((comparison > 0 && f <= comparison) || (comparison < 0 && f >= comparison))
+            return true;
+        return false;
+    }
+
+    public static float Sign(this float signOf)
+    {
+        if (signOf > 0)
+            return 1;
+        else if (signOf == 0)
+            return 0;
+        return -1;
     }
 
     public static bool Outside(this float value, float lowerBounds, float upperBounds)
     {
-        return value < lowerBounds || value > upperBounds;
+        if (value < lowerBounds || value > upperBounds)
+            return true;
+        return false;
     }
 
     /// <summary> Return true if the integer is within the specified bounds [inclusive]. </summary>
     public static bool Inside(this int value, int lowerBounds, int upperBounds)
     {
-        return value >= lowerBounds && value <= upperBounds;
+        if (value >= lowerBounds && value <= upperBounds)
+            return true;
+        return false;
     }
 
     /// <summary> Return true if the float is within the specified bounds [exclusive]. </summary>
     public static bool Inside(this float value, float lowerBounds, float upperBounds)
     {
-        return value > lowerBounds && value < upperBounds;
+        if (value > lowerBounds && value < upperBounds)
+            return true;
+        return false;
     }
 
     public static bool Inside(this Vector3 value, Vector3 lowerBounds, Vector3 upperBounds)
     {
-        return (
-            value.x.Inside(lowerBounds.x, upperBounds.x) &&
+        if (value.x.Inside(lowerBounds.x, upperBounds.x) &&
             value.y.Inside(lowerBounds.y, upperBounds.y) &&
-            value.z.Inside(lowerBounds.z, upperBounds.z)
-        );
+            value.z.Inside(lowerBounds.z, upperBounds.z))
+            return true;
+        return false;
     }
 
     public static bool Outside(this Vector3 value, Vector3 lowerBounds, Vector3 upperBounds)
     {
-        return (
-            value.x.Outside(lowerBounds.x, upperBounds.x) ||
+        if (value.x.Outside(lowerBounds.x, upperBounds.x) ||
             value.y.Outside(lowerBounds.y, upperBounds.y) ||
-            value.z.Outside(lowerBounds.z, upperBounds.z)
-        );
+            value.z.Outside(lowerBounds.z, upperBounds.z))
+            return true;
+        return false;
     }
 
     public static bool Outside2D(this Vector3 value, Vector3 lowerBounds, Vector3 upperBounds)
     {
-        return (
-            value.x.Outside(lowerBounds.x, upperBounds.x) ||
-            value.z.Outside(lowerBounds.z, upperBounds.z)
-        );
+        if (value.x.Outside(lowerBounds.x, upperBounds.x) ||
+            value.z.Outside(lowerBounds.z, upperBounds.z))
+            return true;
+        return false;
+    }
+
+    public static int lerp(this int value, int target, int t)
+    {
+        if (value < target)
+        {
+            value += Mathf.Abs(t);
+            if (value >= target)
+                return target;
+            else
+                return value;
+        }
+        else
+        {
+            value -= Mathf.Abs(t);
+            if (value <= target)
+                return target;
+            else
+                return value;
+        }
+    }
+
+    public static Vector3 Invert(this Vector3 value)
+    {
+        return new Vector3(-value.x, -value.y, -value.z);
+    }
+
+    /// <summary> Replace the Y axis of this vector with the specified value. </summary>
+    public static Vector3 FixedY(this Vector3 value, float newY)
+    {
+        return new Vector3(value.x, newY, value.z);
+    }
+
+    public static Vector3 MultipliedBy(this Vector3 v1, Vector3 v2)
+    {
+        return new Vector3(v1.x * v2.x, v1.y * v2.y, v1.z * v2.z);
+    }
+
+    public static Vector3Int MultipliedBy(this Vector3Int v1, Vector3 v2)
+    {
+        return new Vector3Int(Mathf.FloorToInt(v1.x * v2.x), Mathf.FloorToInt(v1.y * v2.y), Mathf.FloorToInt(v1.z * v2.z));
     }
 }
 
