32b5ef436c82af13f6d524c8a615def18e58eacd
Jordan Epstein <102747419@student.swin.edu.au>
2021-05-06 22:59:26 +1000

    Volume sliders adjust volume logarithmically

--- Capstone-Game/Assets/Scripts/Menus/OptionsMenu.cs
+++ Capstone-Game/Assets/Scripts/Menus/OptionsMenu.cs
@@ -38,12 +38,12 @@ public class OptionsMenu : MonoBehaviour
 
     public void SetMusicVolume(float volume)
     {
-        audioMixer.SetFloat("musicVolume", volume);
+        audioMixer.SetFloat("musicVolume", Mathf.Log10(volume) * 20);
     }
 
     public void SetSFXVolume(float volume)
     {
-        audioMixer.SetFloat("sfxVolume", volume);
+        audioMixer.SetFloat("sfxVolume", Mathf.Log10(volume) * 20);
     }
 
     public void SetResolution(int index)
