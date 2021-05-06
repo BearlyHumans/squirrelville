186e5e92a26db2e153e352a7fc16fd2a583472f0
Jordan Epstein <102747419@student.swin.edu.au>
2021-05-06 22:44:48 +1000

    Resolution dropdown updates resolution

--- Capstone-Game/Assets/Scripts/Menus/OptionsMenu.cs
+++ Capstone-Game/Assets/Scripts/Menus/OptionsMenu.cs
@@ -1,9 +1,40 @@
+using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.Audio;
+using TMPro;
 
 public class OptionsMenu : MonoBehaviour
 {
     public AudioMixer audioMixer;
+    public TMP_Dropdown resolutionDropdown;
+    private Resolution[] resolutions;
+
+    private void Start()
+    {
+        resolutions = Screen.resolutions;
+
+        resolutionDropdown.ClearOptions();
+
+        List<string> options = new List<string>();
+        Resolution currentRes = Screen.currentResolution;
+        int currentResIndex = 0;
+
+        for (int i = resolutions.Length - 1; i >= 0; i--)
+        {
+            Resolution res = resolutions[i];
+
+            options.Add(res.width + "x" + res.height);
+
+            if (res.width == currentRes.width && res.height == currentRes.height)
+            {
+                currentResIndex = i;
+            }
+        }
+
+        resolutionDropdown.AddOptions(options);
+        resolutionDropdown.value = currentResIndex;
+        resolutionDropdown.RefreshShownValue();
+    }
 
     public void SetMusicVolume(float volume)
     {
@@ -13,5 +44,11 @@ public class OptionsMenu : MonoBehaviour
     public void SetSFXVolume(float volume)
     {
         audioMixer.SetFloat("sfxVolume", volume);
+    }
+
+    public void SetResolution(int index)
+    {
+        Resolution res = resolutions[index];
+        Screen.SetResolution(res.width, res.height, Screen.fullScreen);
     }
 }
