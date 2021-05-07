f4ada84b396f851f9775d5fb8b31644e829098e9
Jordan Epstein <102747419@student.swin.edu.au>
2021-05-07 10:43:59 +1000

    Buttons scale when highlighted

--- Capstone-Game/Assets/Scripts/Menus/MenuButton.cs
+++ Capstone-Game/Assets/Scripts/Menus/MenuButton.cs
@@ -7,20 +7,25 @@ public class MenuButton : MonoBehaviour, IPointerEnterHandler, IPointerExitHandl
     private TMP_Text text;
     public Color highlightTextColor;
     private Color defaultTextColor;
+    public float highlightFontScale = 1.0f;
+    private float defaultFontSize;
 
     private void Awake()
     {
         text = GetComponentInChildren<TMP_Text>();
         defaultTextColor = text.color;
+        defaultFontSize = text.fontSize;
     }
 
     public void OnPointerEnter(PointerEventData eventData)
     {
         text.color = highlightTextColor;
+        text.fontSize = defaultFontSize * highlightFontScale;
     }
 
     public void OnPointerExit(PointerEventData eventData)
     {
         text.color = defaultTextColor;
+        text.fontSize = defaultFontSize;
     }
 }
