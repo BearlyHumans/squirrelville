993700f0d837fe9d8442e6c89f206bacd6b1061b
Jordan Epstein <102747419@student.swin.edu.au>
2021-05-29 15:47:57 +1000

    Created food stockpile area

---
+++ Capstone-Game/Assets/Scripts/FoodStockpileArea.cs
@@ -0,0 +1,52 @@
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(MeshRenderer))]
+public class FoodStockpileArea : MonoBehaviour
+{
+    private MeshRenderer mesh;
+    private GameObject[] food;
+
+    private void Start()
+    {
+        mesh = GetComponent<MeshRenderer>();
+        food = GetAllFood();
+    }
+
+    public GameObject[] GetFoodInArea()
+    {
+        List<GameObject> foodInArea = new List<GameObject>();
+
+        foreach (GameObject obj in food)
+        {
+            if (mesh.bounds.Contains(obj.transform.position))
+            {
+                foodInArea.Add(obj);
+            }
+        }
+
+        return foodInArea.ToArray();
+    }
+
+    public int GetFoodCount()
+    {
+        return GetFoodInArea().Length;
+    }
+
+    private GameObject[] GetAllFood()
+    {
+        GameObject[] objects = FindObjectsOfType<GameObject>();
+        List<GameObject> objList = new List<GameObject>();
+        int foodLayer = LayerMask.NameToLayer("Food");
+
+        foreach (GameObject obj in objects)
+        {
+            if (obj.layer == foodLayer)
+            {
+                objList.Add(obj);
+            }
+        }
+
+        return objList.ToArray();
+    }
+}
