dad151fbe8cc2a27398a295dc58ec29e98e45381
tobyamiel <102376266@student.swin.edu.au>
2021-05-29 10:30:35 +1000

    Moved store assets, placed and textured car

---
+++ Capstone-Game/Assets/StoreAssets/BruteForce-GrassShader/Extra/SkyboxGradient.shader
@@ -0,0 +1,68 @@
+﻿// Upgrade NOTE: replaced 'mul(UNITY_MATRIX_MVP,*)' with 'UnityObjectToClipPos(*)'
+
+Shader "Skybox/GradientSkybox"
+{
+	Properties
+	{
+		_Color1("TopColor", Color) = (1, 1, 1, 1)
+		_Color2("HorizonColor", Color) = (1, 1, 1, 1)
+		_Color3("BottomColor", Color) = (1, 1, 1, 1)
+		_Intensity1("IntensityTop", Float) = 1.0		
+		_Intensity2("IntensityMid", Float) = 1.0
+		_Intensity3("IntensityBot", Float) = 1.0
+	}
+
+		CGINCLUDE
+
+#include "UnityCG.cginc"
+
+		struct appdata
+	{
+		float4 position : POSITION;
+		float3 texcoord : TEXCOORD0;
+	};
+
+	struct v2f
+	{
+		float4 position : SV_POSITION;
+		float3 texcoord : TEXCOORD0;
+	};
+
+	half4 _Color1,_Color3,_Color2;
+	half _Intensity1, _Intensity2, _Intensity3;
+
+	v2f vert(appdata v)
+	{
+		v2f o;
+		o.position = UnityObjectToClipPos(v.position);
+		o.texcoord = v.texcoord;
+		return o;
+	}
+
+	half4 frag(v2f i) : COLOR
+	{
+		float p = normalize(i.texcoord).y;
+		float p1 = 1.0f - pow(min(1.0f, 1.0f - p), _Intensity1);
+		float p3 = 1.0f - pow(min(1.0f, 1.0f + p), _Intensity3);
+		float p2 = 1.0f - p1 - p3;
+		return (_Color1 * p1 + _Color2 * p2 + _Color3 * p3) * _Intensity2;
+	}
+
+		ENDCG
+
+		SubShader
+	{
+		Tags{ "RenderType" = "Background" "Queue" = "Background" }
+			Pass
+		{
+			ZWrite Off
+			Cull Off
+			Fog { Mode Off }
+			CGPROGRAM
+			#pragma fragmentoption ARB_precision_hint_fastest
+			#pragma vertex vert
+			#pragma fragment frag
+			ENDCG
+		}
+	}
+}
---
+++ Capstone-Game/Assets/StoreAssets/BruteForce-GrassShader/Scripts/BF_BallPlayer.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BF_BallPlayer : MonoBehaviour
+{
+    public Camera cam;
+    private Rigidbody rb;
+    private Quaternion camRot;
+    private Vector3 moveDirection;
+    private Vector3 inputDirection;
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = this.GetComponent<Rigidbody>();
+        if(cam == null)
+        {
+            cam = Camera.main;
+        }
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        inputDirection = Vector3.zero;
+        if (Input.GetKey(KeyCode.Q)|| Input.GetKey(KeyCode.A))
+        {
+            inputDirection += new Vector3(0, 0, 1);
+        }
+        if (Input.GetKey(KeyCode.D))
+        {
+            inputDirection += new Vector3(0, 0, -1);
+        }
+        if (Input.GetKey(KeyCode.Z) || Input.GetKey(KeyCode.W))
+        {
+            inputDirection += new Vector3(1, 0, 0);
+        }
+        if (Input.GetKey(KeyCode.S))
+        {
+            inputDirection += new Vector3(-1, 0, 0);
+        }
+        MoveBall();
+    }
+
+    private void MoveBall()
+    {
+        camRot = Quaternion.AngleAxis(cam.transform.rotation.eulerAngles.y, Vector3.up);
+        moveDirection = camRot * new Vector3(Mathf.Clamp(inputDirection.x * 2, -1, 1), 0, Mathf.Clamp(inputDirection.z * 2, -1, 1));
+        rb.AddTorque(moveDirection*7.5f);
+    }
+}
