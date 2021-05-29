95d27bbfa7a5e41889240d22f4fe97c7bf910e62
Jordan Epstein <102747419@student.swin.edu.au>
2021-05-29 15:55:41 +1000

    Display food collected count on HUD

--- Capstone-Game/Assets/Scripts/FoodStockpileArea.cs
+++ Capstone-Game/Assets/Scripts/FoodStockpileArea.cs
@@ -1,16 +1,23 @@
 using System.Collections.Generic;
 using UnityEngine;
+using TMPro;
 
 [RequireComponent(typeof(MeshRenderer))]
 public class FoodStockpileArea : MonoBehaviour
 {
     private MeshRenderer mesh;
     private GameObject[] food;
+    public TMP_Text label;
 
     private void Start()
     {
         mesh = GetComponent<MeshRenderer>();
         food = GetAllFood();
+    }
+
+    private void Update()
+    {
+        label.text = $"Food collected: {GetFoodCount()}";
     }
 
     public GameObject[] GetFoodInArea()
