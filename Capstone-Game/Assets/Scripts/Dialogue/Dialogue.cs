5bf523c602306f51e56282c31915a6a6f37f185e
Jordan Epstein <102747419@student.swin.edu.au>
2021-08-16 13:05:31 +1000

    Can move to next subsequent sentences

--- Capstone-Game/Assets/Scripts/Dialogue/Dialogue.cs
+++ Capstone-Game/Assets/Scripts/Dialogue/Dialogue.cs
@@ -1,5 +1,4 @@
 using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 using TMPro;
 
@@ -7,21 +6,54 @@ public class Dialogue : MonoBehaviour
 {
     public TMP_Text text;
     [TextArea] public string[] sentences;
-    private uint index;
+    private int index = -1;
     public float typingSpeed;
+    private bool isTyping = false;
+    private Coroutine typingCoroutine;
 
     private void Start()
     {
-        text.text = "";
-        StartCoroutine(Type());
+        NextSentence();
     }
 
-    IEnumerator Type()
+    private IEnumerator Type()
     {
+        isTyping = true;
+
         foreach (char letter in sentences[index].ToCharArray())
         {
             text.text += letter;
             yield return new WaitForSeconds(typingSpeed);
+        }
+
+        isTyping = false;
+    }
+
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Return))
+        {
+            if (isTyping)
+            {
+                StopCoroutine(typingCoroutine);
+                isTyping = false;
+                text.text = sentences[index];
+            }
+            else
+            {
+                NextSentence();
+            }
+        }
+    }
+
+    public void NextSentence()
+    {
+        text.text = "";
+
+        if (index < sentences.Length - 1)
+        {
+            index++;
+            typingCoroutine = StartCoroutine(Type());
         }
     }
 }
